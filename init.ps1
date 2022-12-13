d116a66699d2458d85bb9efe7eb372a6bbedc69d
Antony Baasandorj <antony.baasan@gmail.com>
2022-12-13 10:22:51 -0500

    add todo list

--- init.ps1
+++ init.ps1
@@ -1,4 +1,17 @@
 
-$secrets = Get-Content .\subcriptly-secrets.json -Raw | ConvertFrom-Json
+param (
+    [string]
+    $secrets_file = '.\subcriptly-secrets.json'
+)
 
+if (-not(Test-Path $secrets_file)) {
+    Write-Error "The secrets file is missing!"
+    exit
+}
+
+$secrets = Get-Content $secrets_file -Raw | ConvertFrom-Json
+#test if secrets file read correctly
 Write-Output $secrets.name
+
+#TODO: Setup ansible
+#TODO: Validate Azure credentials
