9ff8de4cb9cbe85e9821cef8921bfb7f7c2b7a66
Antony Baasandorj <abaasandorj@microsoft.com>
2023-01-20 21:58:53 -0500

    add models project

---
+++ backend/models/src/test/java/com/subcriptly/models/ModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subcriptly.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
