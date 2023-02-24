69c1170db5c254aa966a443291fd87b75f506d83
Antony Baasandorj <abaasandorj@microsoft.com>
2023-02-24 10:35:21 -0500

    add migration - item tables

---
+++ backend/dbmanager/src/main/resources/db/migration/V4__create_item_tables.sql
@@ -0,0 +1,34 @@
+-- Item table
+create table ITEM (
+    ID integer primary key AUTO_INCREMENT,
+    NAME nvarchar(200) not null,
+    DESCRIPTION nvarchar(2000) null,
+    PROJECT_ID nvarchar(100) not null,
+    TYPE integer
+);
+
+insert into ITEM (ID, NAME, DESCRIPTION, PROJECT_ID)
+values (1, 'Application 1', 'Dddd', 'abc123');
+insert into ITEM (ID, NAME, DESCRIPTION, PROJECT_ID)
+values (2, 'Game 2', 'Dddd', 'abc123');
+
+-- Item Type table
+create table ITEM_TYPE (
+    ID integer primary key AUTO_INCREMENT,
+    NAME nvarchar(200) not null
+);
+
+insert into ITEM_TYPE (NAME)
+values ('Application');
+insert into ITEM_TYPE (NAME)
+values ('Database');
+
+-- Item Artifacts
+create table ITEM_ARTIFACT (
+    ID integer primary key AUTO_INCREMENT,
+    ITEM_ID nvarchar(100) not null,
+    LOCATION nvarchar(200) not null
+);
+
+insert into ITEM_ARTIFACT (ITEM_ID, LOCATION)
+values (1, 'C:\\temp\\web1');
